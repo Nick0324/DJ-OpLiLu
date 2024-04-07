bde8746d0ad88d10d4ddc78be7d34f58078aed4d
EusebiuOprea <eusebiuoprea243@gmail.com>
2024-04-07 20:29:09 +0300

    add disconnect command

--- command_implementations.py
+++ command_implementations.py
@@ -12,3 +12,13 @@ async def join(ctx):
         await functions.join_channel(ctx)
     else:
         await ctx.send("You don't have the required role for that")
+
+
+@client.command(name="disconnect", pass_ctx=True)
+async def disconnect(ctx):
+    role = discord.utils.get(ctx.author.roles, name="dj")
+    if role:
+        vc = ctx.message.guild.voice_client
+        await vc.disconnect()
+    else:
+        await ctx.send("I'm not connected to any channels")
