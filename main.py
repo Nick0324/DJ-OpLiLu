34ab57636b6b579523cf853c833e8f58d7ad3e0e
Nicu Lupu <nicolae.lupu10@gmail.com>
2024-04-07 13:45:22 +0300

    set up bot

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
+import discord
+from discord.ext import commands
+
+client = commands.Bot(intents=discord.Intents.default(), command_prefix="!")
+
+
 def test_main():
     print("Hello")
+
+
+file = open('token.txt', 'r')
+client.run(file.readline())
+file.close()
