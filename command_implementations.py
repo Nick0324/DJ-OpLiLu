37a22931cd4e6bc3056b525f61c56bae36bf6bb6
Lisaru <Lisaru@EMI>
2024-04-07 20:41:39 +0300

    join command error message

--- command_implementations.py
+++ command_implementations.py
@@ -2,13 +2,25 @@ import functions
 import discord
 from discord.ext import commands
 
+def is_connected(ctx):
+    voice_client = get(ctx.bot.voice_clients, guild=ctx.guild)
+    return voice_client and voice_client.is_connected()
+
 intents = discord.Intents.all()
 
 client = commands.Bot(intents=intents, command_prefix="!")
 @client.command()
 async def join(ctx):
+
+    voice = discord.utils.get(client.voice_clients,guild=ctx.guild)
     role = discord.utils.get(ctx.author.roles, name="dj")
+
     if role:
-        await functions.join_channel(ctx)
+        if voice == None:
+            await functions.join_channel(ctx)
+        else:
+            await ctx.send(f"I'm already connected to **{ctx.author.voice.channel}**!")
     else:
         await ctx.send("You don't have the required role for that")
+
+
--- functions.py
+++ functions.py
@@ -1,2 +1,4 @@
 async def join_channel(ctx):
     await ctx.author.voice.channel.connect()
+    await ctx.send(f"Joined **{ctx.author.voice.channel}**")
+
