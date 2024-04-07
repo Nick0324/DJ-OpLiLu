f3d17c1d1d559affb645810b2def446b98d8ba2f
Lisaru <Lisaru@EMI>
2024-04-07 21:01:11 +0300

    join command error message

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 async def join_channel(ctx):
     await ctx.author.voice.channel.connect()
+    await ctx.send(f"Joined **{ctx.author.voice.channel}**")
 async def disconnect_channel(ctx):
     await ctx.author.voice.channel.disconnect()
