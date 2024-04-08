053af25b79cd3f3de27ad30196b002d8a4bd2bd8
Nicu Lupu <nicolae.lupu10@gmail.com>
2024-04-08 22:18:00 +0300

    Add play functionality

--- command_implementations.py
+++ command_implementations.py
@@ -5,6 +5,8 @@ from discord.ext import commands
 intents = discord.Intents.all()
 
 client = commands.Bot(intents=intents, command_prefix="!")
+
+
 @client.command()
 async def join(ctx):
     role = discord.utils.get(ctx.author.roles, name="dj")
@@ -22,3 +24,28 @@ async def disconnect(ctx):
         await vc.disconnect()
     else:
         await ctx.send("I'm not connected to any channels")
+
+
+@client.command()
+async def play(ctx, *args):
+    query = " ".join(args)
+    try:
+        voice_channel = ctx.author.voice.channel
+    except:
+        await ctx.send("```You need to connect to a voice channel first!```")
+        return
+    if functions.is_paused:
+        functions.vc.resume()
+    else:
+        song = functions.search_yt(query)
+        if type(song) == type(True):
+            await ctx.send(
+                "```Could not download the song. Incorrect format try another keyword. This could be due to playlist or a livestream format.```")
+        else:
+            if functions.is_playing:
+                await ctx.send(f"**#{len(functions.music_queue) + 2} -'{song['title']}'** added to the queue")
+            else:
+                await ctx.send(f"**'{song['title']}'** added to the queue")
+            functions.music_queue.append([song, voice_channel])
+            if not functions.is_playing:
+                await functions.play_music(ctx, client)
--- functions.py
+++ functions.py
@@ -1,4 +1,82 @@
+import asyncio
+
+import discord
+import yt_dlp
+from youtubesearchpython import VideosSearch
+
+is_playing = False
+is_paused = False
+
+music_queue = []
+YDL_OPTIONS = {'format': 'bestaudio/best'}
+FFMPEG_OPTIONS = {'options': '-vn'}
+
+ytdl = yt_dlp.YoutubeDL(YDL_OPTIONS)
+
+vc = None
+
+
 async def join_channel(ctx):
     await ctx.author.voice.channel.connect()
+
+
 async def disconnect_channel(ctx):
     await ctx.author.voice.channel.disconnect()
+
+
+def search_yt(item):
+    if item.startswith("https://"):
+        title = ytdl.extract_info(item, download=False)["title"]
+        return {'source': item, 'title': title}
+
+    search = VideosSearch(item, limit=1)
+    return {'source': search.result()["result"][0]["link"], 'title': search.result()["result"][0]["title"]}
+
+
+async def play_next(client):
+    global is_playing
+    global vc
+    if len(music_queue) > 0:
+        is_playing = True
+
+        # get the first url
+        m_url = music_queue[0][0]['source']
+
+        # remove the first element as you are currently playing it
+        music_queue.pop(0)
+        loop = asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(m_url, download=False))
+        song = data['url']
+        vc.play(discord.FFmpegPCMAudio(song, executable="ffmpeg.exe", before_options="-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5",options="-vn"),
+                     after=lambda e: asyncio.run_coroutine_threadsafe(play_next(client), client.loop))
+    else:
+        is_playing = False
+
+
+async def play_music(ctx, client):
+    if len(music_queue) > 0:
+        global is_playing
+        is_playing = True
+
+        m_url = music_queue[0][0]['source']
+        # try to connect to voice channel if you are not already connected
+        global vc
+        if vc is None or not vc.is_connected():
+            vc = await music_queue[0][1].connect()
+
+            # in case we fail to connect
+            if vc is None:
+                await ctx.send("```Could not connect to the voice channel```")
+                return
+        else:
+            await vc.move_to(music_queue[0][1])
+        # remove the first element as you are currently playing it
+        music_queue.pop(0)
+        loop = asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(m_url, download=False))
+        song = data['url']
+        vc.play(discord.FFmpegPCMAudio(song, executable="ffmpeg.exe", **FFMPEG_OPTIONS),
+                     after=lambda e: asyncio.run_coroutine_threadsafe(play_next(client), client.loop))
+
+    else:
+        is_playing = False
