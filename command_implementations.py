878bb708605e494ef98860f1ae32ae3c1ef26309
Lisaru <Lisaru@EMI>
2024-04-15 22:10:55 +0300

    Dissconnect if idle functionality

--- command_implementations.py
+++ command_implementations.py
@@ -1,10 +1,18 @@
 import functions
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
+import asyncio
 
 intents = discord.Intents.all()
 
 client = commands.Bot(intents=intents, command_prefix="!")
+
+@client.event
+async def on_ready():
+    print("Bot is ready")
+    check_idle.start()
+    check_alone.start()
+
 @client.command(name="join", pass_ctx=True)
 async def join(ctx):
 
@@ -28,3 +36,18 @@ async def disconnect(ctx):
         await vc.disconnect()
     else:
         await ctx.send("I'm not connected to any channels")
+
+@tasks.loop(seconds=30)
+async def check_idle():
+    print("Timer check idle")
+    for vc in client.voice_clients: # Check for every channel that the bot is connected to
+        if not vc.is_playing(): # If not playing audio
+            await vc.disconnect()
+
+@tasks.loop(seconds=15)
+async def check_alone():
+    print("Timer check alone")
+    for vc in client.voice_clients:  # Check for every channel that the bot is connected to
+        if len(vc.channel.members) == 1 and client.user in vc.channel.members:  # If only the bot is connected
+            await vc.disconnect()
+
