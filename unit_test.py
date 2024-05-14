57ad38675825aacdb3352501d93c6910bb9ef9c3
EusebiuOprea <116063106+EusebiuOprea@users.noreply.github.com>
2024-05-14 10:06:03 +0300

    Update unit_test.py

--- unit_test.py
+++ unit_test.py
@@ -10,16 +10,13 @@ class TestMusicCommands(unittest.TestCase):
         self.ctx.voice_client = self.player
         self.player.playing = True
         await skip(self.ctx)
-        # Assert that the skip command correctly skips the song
         self.player.skip.assert_called_once_with(force=True)
         self.ctx.message.add_reaction.assert_called_once_with("\u2705")
     async def test_resume(self):
-        # Mocking necessary attributes and methods
         self.ctx.voice_client = self.player
         self.player.paused = True
         await pause_resume(self.ctx)
         self.player.pause.assert_called_once_with(False)
         self.ctx.message.add_reaction.assert_called_once_with("\u2705")
-
 if __name__ == '__main__':
     unittest.main()
